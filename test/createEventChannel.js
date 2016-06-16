2a3157c8777a4219728824774327309c0550dd96
Stian Didriksen <stipsan@gmail.com>
2016-06-16 23:22:33 +0200

    implement first test

--- test/createEventChannel.js
+++ test/createEventChannel.js
@@ -1,7 +1,29 @@
 import expect from 'expect'
 
-import createEventChannel from '../src'
+import { createEventChannel } from '../src'
 
 describe('createEventChannel', () => {
-  it('should create an event channel')
+  const socket = {
+    on(event, listener) {
+      this.listener = listener
+    },
+    off() {
+      delete this.listener
+    },
+    emit(event, data) {
+      this.listener(data)
+    },
+  }
+  const chan = createEventChannel(socket)
+
+  it('should create an event channel', () => {
+    const actual = []
+    const action = { type: 'TEST', payload: { foo: 'bar' } }
+
+    chan.take((ac) => actual.push(ac))
+    socket.emit('dispatch', action)
+    expect(actual).toContain(action)
+    socket.emit('dispatch', action)
+    expect(actual.length).toBe(1, 'eventChannel should only notify once')
+  })
 })
