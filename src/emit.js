4aaffd3ba5ad8d8d4f5ba2663a9ba542fbba9dfc
Stian Didriksen <stipsan@gmail.com>
2016-06-18 00:17:31 +0200

    remove constants, lets keep this low-level
    these actions will be dispatched by higher order workers and watchers
    instead so that you can opt-in to them instead of being forced to
    opt-out.
    People almost never change the default settings and this could cause a
    performance hit on many use cases if the usage is unclear.

--- src/emit.js
+++ src/emit.js
@@ -1,10 +1,5 @@
 import { cps, fork } from 'redux-saga/effects'
 
-import { sym } from './utils'
-
-export const EMIT_TIMEOUT = sym('EMIT_TIMEOUT')
-export const EMIT_ERROR = sym('EMIT_ERROR')
-
 export function *handleEmit(socket, action, event = 'dispatch') {
   yield cps([socket, socket.emit], event, action)
 }
