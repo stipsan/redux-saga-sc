a7e25ca02520a41a072870f033dd7ea5bc5c0652
Stian Didriksen <stipsan@gmail.com>
2016-06-18 02:04:06 +0200

    finished request effect

--- src/request.js
+++ src/request.js
@@ -1,7 +1,21 @@
-import { fork } from 'redux-saga/effects'
+import { delay } from 'redux-saga'
+import { call, put, race, take } from 'redux-saga/effects'
 
 import { handleEmit } from './emit'
 
-export function *request(socket, action, event) {
-  yield fork(handleEmit, socket, action, event)
+export function *deadline(timeRemaining) {
+  yield call(delay, timeRemaining)
+  const error = new Error('Socket request timed out waiting for a response')
+  error.name = 'SocketTimeoutError'
+  throw error
+}
+
+export function *request(socket, action, event, timeRemaining = socket.ackTimeout) {
+  yield call(handleEmit, socket, action, event)
+  const { payload: { successType, failureType } } = action
+  const { response } = yield race({
+    response: take([successType, failureType]),
+    timeout: call(deadline, timeRemaining),
+  })
+  yield put(response)
 }
--- test/request.js
+++ test/request.js
@@ -1,8 +1,10 @@
 import expect from 'expect'
-import { fork } from 'redux-saga/effects'
+import { delay } from 'redux-saga'
+import { call, put, race, take } from 'redux-saga/effects'
 
 import { request } from '../src'
 import { handleEmit } from '../src/emit'
+import { deadline } from '../src/request'
 
 describe('request', () => {
   const type = 'SERVER_REQUEST'
@@ -17,15 +19,52 @@ describe('request', () => {
   }
   const event = 'dispatch'
   const socket = {
+    ackTimeout: 1000,
     emit() {},
   }
+  const iterator = request(socket, action, event)
 
-  it('should fork emit to handleEmit worker so it does not block', () => {
-    const iterator = request(socket, action, event)
+  it('should call handleEmit directly so it waits until receiver report action received', () => {
     expect(
       iterator.next().value
     ).toEqual(
-      fork(handleEmit, socket, action, event)
+      call(handleEmit, socket, action, event)
+    )
+  })
+
+  it('should start a timeout race', () => {
+    expect(
+      iterator.next().value
+    ).toEqual(
+      race({
+        response: take([successType, failureType]),
+        timeout: call(deadline, socket.ackTimeout),
+      })
+    )
+  })
+
+  it('should throw a SocketTimeoutError if response fails to meet the deadline', () => {
+    const requestDeadline = deadline(socket.ackTimeout)
+    expect(
+      requestDeadline.next().value
+    ).toEqual(
+      call(delay, socket.ackTimeout)
+    )
+
+    expect(() => {
+      requestDeadline.next()
+    }).toThrow(/request timed out/)
+  })
+
+  it('should put the server response if it wins the race', () => {
+    const successAction = {
+      type: successType,
+      payload: {},
+    }
+    expect(
+      iterator.next({ response: successAction }).value
+    ).toEqual(
+      put(successAction)
     )
   })
 })
