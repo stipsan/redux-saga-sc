9474932f737d3f4730c8b6048dbc51ae89eeb34e
Stian Didriksen <stipsan@gmail.com>
2016-06-18 19:58:51 +0200

    watchRequests watcher added

--- src/watchers.js
+++ src/watchers.js
@@ -1,11 +1,27 @@
-import { take, put, call } from 'redux-saga/effects'
+import { channel } from 'redux-saga'
+import { call, fork, put, take } from 'redux-saga/effects'
 
+import { REQUEST } from './actions'
 import { createEventChannel } from './eventChannel'
+import { processRequest } from './workers'
 
 export function *watchRemote(socket, event = 'dispatch') {
   const chan = yield call(createEventChannel, socket, event)
   while (true) { // eslint-disable-line
     const action = yield take(chan)
     yield put(action)
+  }
+}
+
+export function *watchRequests(socket, concurrency = 3, retries = 5) {
+  const chan = yield call(channel)
+
+  for (let i = 0; i < concurrency; i++) {
+    yield fork(processRequest, socket, chan, retries)
+  }
+
+  while (true) {
+    const payload = yield take(REQUEST)
+    yield put(chan, payload)
   }
 }
--- test/watchers/watchRequests.js
+++ test/watchers/watchRequests.js
@@ -1,5 +1,31 @@
+import expect from 'expect'
+import { channel } from 'redux-saga'
+import { call, fork } from 'redux-saga/effects'
+
+import { watchRequests } from '../../src'
+import { processRequest } from '../../src/workers'
+
 describe('watchRequests', () => {
-  it('should setup a channel to queue incoming requests')
-  it('should create 3 worker "threads"')
+  const socket = {
+    emit() {},
+  }
+  const workers = 2
+  const retries = 5
+  const chan = channel()
+  const iterator = watchRequests(socket, workers, retries)
+  it('should setup a channel to queue incoming requests', () => {
+    expect(
+      iterator.next().value
+    ).toEqual(
+      call(channel)
+    )
+  })
+  it('should create 2 worker "threads"', () => {
+    expect(
+      iterator.next(chan).value
+    ).toEqual(
+      fork(processRequest, socket, chan, retries)
+    )
+  })
   it('should take requests and put them on the channel')
 })
