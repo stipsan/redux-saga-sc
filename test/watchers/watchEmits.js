cd663b1518e6718001a45cf37b5b218261129530
Stian Didriksen <stipsan@gmail.com>
2016-06-18 14:19:24 +0200

    add pending tests for watchers

---
+++ test/watchers/watchEmits.js
@@ -0,0 +1,9 @@
+describe('watchEmits', () => {
+  it('should create a channel for emit actions')
+  it('should take from the channel')
+
+  it('should dispatch a blocking call')
+  // doing it in a blocking way helps queue up the buffer if the network goes down
+  // reducing the amount of failed emits while we wait for stuff to come back online
+  // this behavior might change once real error handling is implemented
+})
---
+++ test/watchers/watchRequests.js
@@ -0,0 +1,5 @@
+describe('watchRequests', () => {
+  it('should setup an action channel to queue incoming requests')
+  it('should create 3 worker "threads"')
+  it('should take requests and put them on the channel')
+})
